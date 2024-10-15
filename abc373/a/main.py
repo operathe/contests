ca0224b77c20718111aef26b41e54db799c0d730
operathe <operathe11@gmail.com>
2024-10-15 19:13:03 +0900

    abc374

--- abc373/a/main.py
+++ abc373/a/main.py
@@ -1,14 +1,14 @@
-# N = 12
-#
-# ans = 0
-#
-# for i in range(1, N + 1):
-#     S = input()
-#     if len(S) == i:
-#         ans += 1
-#
-# print(ans)
+N = 12
+
+ans = 0
+
+for i in range(1, N + 1):
+    S = input()
+    if len(S) == i:
+        ans += 1
+
+print(ans)
 
 
-s = [input() for _ in range(12)]
-print(sum([len(s[i]) == i + 1 for i in range(12)]))
+# s = [input() for _ in range(12)]
+# print(sum([len(s[i]) == i + 1 for i in range(12)]))
